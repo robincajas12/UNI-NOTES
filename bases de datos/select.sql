4ab68cfdec9f8465ae83252bdd13b35db7072978
Robinson Cajas <75333543+robincajas12@users.noreply.github.com>
2024-11-13 15:40:23 -0500

    Update select.sql

--- bases de datos/select.sql
+++ bases de datos/select.sql
@@ -6,3 +6,8 @@
 -- select dni, apell1, apell2, sueldo, (sueldo * @iessImp) as iess, sueldo - (sueldo * @iessImp) as sueldo_recibir from empleado
 select nombre,apell1,apell2,dni,direccion,sexo,sueldo,nrp as xd from empleado
  where (dep=4 and sueldo > 25000)
+select * from producto;
+select * from fabricante ;
+select producto.nombre as p_nombre, producto.precio, fabricante.nombre as f_nombre from producto cross join fabricante on producto.id_fabricante = fabricante.id;
+
+-- ddl para objetos, dml para datos
