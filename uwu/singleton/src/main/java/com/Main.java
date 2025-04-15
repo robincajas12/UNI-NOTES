e6b975d861ed797ff2fe2f5b41209e1612857f28
Robinson Cajas <75333543+robincajas12@users.noreply.github.com>
2025-04-15 10:14:14 -0500

    Add files via upload

--- uwu/singleton/src/main/java/com/Main.java
+++ uwu/singleton/src/main/java/com/Main.java
@@ -2,20 +2,15 @@ package com;
 
 import java.sql.Connection;
 
+import com.connection.DbConfig;
+import com.connection.Singleton;
+
 public class Main {
-	private static Connection connection = null;
-	public static Main get()
-	{
-		return null;
-	}
-	private Main()
-	{
-		
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("UwU");
-		Main.get();
+		Singleton.getInstante();
+		Singleton.getInstante();
 	}
 
 }
